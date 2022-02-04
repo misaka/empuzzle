7af6a7a15390da04ccb4050d4dc45ad04119f8a4
Misha Gorodnitzky <misaka@pobox.com>
2022-02-04 01:12:26 +0000

    convert maths_grid form to use form_for

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -4,21 +4,22 @@ class PuzzlesController < ApplicationController
   def maths_grid
     @puzzle = MathsGridPuzzle.new(maths_grid_create_params)
     @puzzle.validate!
-    @reward = params[:reward] || ""
+    @reward = params[:reward] || ''
   end
 
   private
 
   def maths_grid_create_params
-    params.permit(
-      :rows, :columns,
-      :factors_from, :factors_to, :factors_count_min, :factors_count_max,
-      :dividends_from, :dividends_to, :divisors_from, :divisors_to,
-    )
+    params
+      .permit(:maths_grid_puzzle)
+      .permit(
+        :rows, :columns,
+        :factors_from, :factors_to, :factors_count_min, :factors_count_max,
+        :dividends_from, :dividends_to, :divisors_from, :divisors_to,
+      )
   end
 
   def transform_values_for_keys(p, *keys, &block)
     p.merge(p.slice(*keys).transform_values(&block))
   end
-
 end
