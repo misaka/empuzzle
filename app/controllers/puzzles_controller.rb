f026b781b0aadd0e0d05bb9fe79e6456b704ece2
Misha Gorodnitzky <misaka@pobox.com>
2022-02-04 00:25:32 +0000

    add validations for maths grid params

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -3,21 +3,16 @@ class PuzzlesController < ApplicationController
 
   def maths_grid
     @puzzle = MathsGridPuzzle.new(maths_grid_create_params)
+    @puzzle.validate!
   end
 
   private
 
   def maths_grid_create_params
-    transform_values_for_keys(
-      params.permit(
-        :rows, :columns,
-        :factors_from, :factors_to, :factors_count_min, :factors_count_max,
-        :dividends_from, :dividends_to, :divisors_from, :divisors_to,
-      ),
+    params.permit(
       :rows, :columns,
       :factors_from, :factors_to, :factors_count_min, :factors_count_max,
       :dividends_from, :dividends_to, :divisors_from, :divisors_to,
-      &:to_i
     )
   end
 
--- app/puzzles/maths_grid_puzzle.rb
+++ app/puzzles/maths_grid_puzzle.rb
@@ -1,45 +1,37 @@
 class MathsGridPuzzle
   include ActiveModel::API
+  include ActiveModel::Attributes
 
-  PREDEFINED_CONFIGS = {
-    default: {
-      rows: 4,
-      columns: 6,
+  attribute :rows,              :integer, default: 4
+  attribute :columns,           :integer, default: 6
+  attribute :dividends_from,    :integer, default: 1
+  attribute :dividends_to,      :integer, default: 1000
+  attribute :divisors_from,     :integer, default: 1
+  attribute :divisors_to,       :integer, default: 20
+  attribute :factors_from,      :integer, default: 1
+  attribute :factors_to,        :integer, default: 12
+  attribute :factors_count_min, :integer, default: 2
+  attribute :factors_count_max, :integer, default: 3
 
-      dividends_from: 1,
-      dividends_to: 1000,
-
-      divisors_from: 1,
-      divisors_to: 20,
-
-      factors_from: 1,
-      factors_to: 12,
-
-      factors_count_min: 2,
-      factors_count_max: 3,
-    }
-  }
-
-  attr_accessor :rows, :columns,
-                :dividends_from, :dividends_to,
-                :divisors_from, :divisors_to,
-                :factors_from, :factors_to,
-                :factors_count_min, :factors_count_max
+  validates_presence_of :rows, :columns
+  validates_presence_of :dividends_from, :dividends_to
+  validates_presence_of :divisors_from, :divisors_to
+  validates_presence_of :factors_from, :factors_to
+  validates_presence_of :factors_count_min, :factors_count_max
+  validate :dividends_from_not_greater_than_to
+  validate :divisors_from_not_greater_than_to
+  validate :factors_from_not_greater_than_to
+  validate :factors_count_min_not_greater_than_max
 
   def initialize(config_params = {})
     super
-
-    config = PREDEFINED_CONFIGS[:default]
-    config.each do |attr, value|
-      send("#{attr}=", value) unless instance_variable_defined? "@#{attr}"
-    end
 
     @random = Random.new
   end
 
   def cells
-    @cells ||= @rows.times.map do |row|
-      @columns.times.map do |col|
+    @cells ||= rows.times.map do |row|
+      columns.times.map do |col|
         case random_cell_type
         when :division then generate_division_cell
         when :multiplication then generate_multiplication_cell
@@ -49,6 +41,22 @@ class MathsGridPuzzle
   end
 
   private
+
+  def dividends_from_not_greater_than_to
+    errors.add(:dividends_from, "cannot be greater than dividends_to") if dividends_from > dividends_to
+  end
+
+  def divisors_from_not_greater_than_to
+    errors.add(:divisors_from, "cannot be greater than divisors_to") if divisors_from > divisors_to
+  end
+
+  def factors_from_not_greater_than_to
+    errors.add(:factors_from, "cannot be greater than factors_to") if factors_from > factors_to
+  end
+
+  def factors_count_min_not_greater_than_max
+    errors.add(:factors_count_min, "cannot be greater than factors_to") if factors_count_min > factors_count_max
+  end
 
   def generate_division_cell
     OpenStruct.new(
@@ -85,18 +93,18 @@ class MathsGridPuzzle
   end
 
   def factors_count
-    random_in_range(@factors_count_min, @factors_count_max)
+    random_in_range(factors_count_min, factors_count_max)
   end
 
   def factor
-    random_in_range(@factors_from, @factors_to)
+    random_in_range(factors_from, factors_to)
   end
 
   def dividend
-    random_in_range(@dividends_from, @dividends_to)
+    random_in_range(dividends_from, dividends_to)
   end
 
   def divisor
-    random_in_range(@divisors_from, @divisors_to)
+    random_in_range(divisors_from, divisors_to)
   end
 end
