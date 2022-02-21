20252cb1ec86ccbcf9c0e4f786c6033b6fdf9eac
Misha Gorodnitzky <misaka@pobox.com>
2022-02-21 23:26:22 +0000

    add form for number lines

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -7,6 +7,12 @@ class PuzzlesController < ApplicationController
     @reward = params[:reward] || ''
   end
 
+  def maths_grid_2
+    @puzzle = Puzzles::MathsGrid2.new(maths_grid_2_create_params[:puzzles_maths_grid])
+    @puzzle.validate!
+    @reward = params[:reward] || ''
+  end
+
   def number_line_maths
     @puzzle = Puzzles::NumberLineMaths.new(number_line_maths_create_params[:puzzles_number_line_maths])
     @puzzle.validate!
@@ -30,10 +36,25 @@ class PuzzlesController < ApplicationController
       )
   end
 
+  def maths_grid_2_create_params
+    params
+      .permit(
+        puzzles_maths_grid: [
+          :rows, :columns,
+          :factors_from, :factors_to, :factors_count_min, :factors_count_max,
+          :dividends_from, :dividends_to, :divisors_from, :divisors_to,
+          :addition_count_min, :addition_count_max,
+          :addition_from, :addition_to,
+          :subtraction_count_min, :subtraction_count_max,
+          :subtraction_from, :subtraction_to,
+        ]
+      )
+  end
+
   def number_line_maths_create_params
     params
       .permit(
-        number_line_maths_puzzle: [
+        puzzles_number_line_maths: [
           :rows,
           :reward,
           :line_range_from, :line_range_to,
