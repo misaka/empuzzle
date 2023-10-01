a9e879eedeaafe37ff4e319199eab75f0c96c5f7
Misha Gorodnitzky <misaka@dmfg.io>
2023-10-02 00:27:50 +0100

    Add test to ensure we can generate maths_grids

---
+++ spec/models/puzzles/maths_grid_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe Puzzles::MathsGrid do
+  it "does not fail to generate puzzles even with a low entropy puzzle" do
+    # This is a pretty brute-force test, but it's pretty quick. The point is we
+    # should be able to generate a bunck of puzzles withou triggering errors
+    # where equations can't be generated due to the entropy restrictions of the
+    # puzzle settings (i.e. the number of available puzzle combinations is too
+    # low and the puzzle generator gives up to avoid getting into an infinite
+    # loop).
+    #
+    # Also, the following URL currently generates an error:
+    #
+    #   http://localhost:3000/puzzles/maths_grid/19498556
+    #
+    # But although just testing this could be handy, it would be too easy for
+    # this seed to start passing the test suddenly, which is why we use the
+    # brute force method here.
+    expect {
+      100.times { |_n| described_class.new.generate_data }
+    }.not_to raise_error
+  end
+end
