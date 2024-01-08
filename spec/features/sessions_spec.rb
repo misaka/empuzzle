870c1a011071f834a03746407024e2b8819fa6f6
Misha Gorodnitzky <misaka@dmfg.io>
2024-01-08 01:23:07 +0000

    Add puzzle authorisation spec

---
+++ spec/features/sessions_spec.rb
@@ -0,0 +1,64 @@
+require "rails_helper"
+
+RSpec.feature "sessions" do
+  scenario "accessing other puzzles" do
+    given_i_have_a_new_session
+    when_i_go_to_the_site_main_page
+    then_i_see_no_puzzles
+
+    when_i_create_a_new_puzzle
+    and_go_back_to_the_home_page
+    then_i_see_my_one_puzzle
+
+    given_i_have_a_new_session
+    when_i_go_to_the_site_main_page
+    then_i_see_no_puzzles
+
+    when_i_create_a_new_puzzle
+    and_go_back_to_the_home_page
+    then_i_see_my_one_puzzle
+
+    when_i_try_to_access_the_first_puzzle
+    then_i_get_an_authorisation_error
+  end
+
+  def given_i_have_a_new_session
+    Capybara.reset_session!
+  end
+
+  def when_i_go_to_the_site_main_page
+    visit "/puzzles"
+  end
+
+  def then_i_see_no_puzzles
+    expect(page).not_to have_css("a", text: /Maths Grid Puzzle/)
+  end
+
+  def when_i_create_a_new_puzzle
+    click_button "New Maths Grid"
+    click_button "Generate sheet"
+    @last_puzzle_path = @puzzle_path
+    @puzzle_path = page.current_path
+  end
+
+  def and_go_back_to_the_home_page
+    click_link "Home"
+  end
+
+  def then_i_see_my_one_puzzle
+    expect(page.all("a", text: /Maths Grid Puzzle/).count).to eq 1
+    expect(page.first("a", text: /Maths Grid Puzzle/).native["href"]).to eq(
+      @puzzle_path
+    )
+  end
+
+  def when_i_try_to_access_the_first_puzzle
+    visit @last_puzzle_path
+  rescue StandardError => e
+    @last_exception = e
+  end
+
+  def then_i_get_an_authorisation_error
+    expect(@last_exception).to be_a(ActiveRecord::RecordNotFound)
+  end
+end
