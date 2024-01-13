8119a970d027dd6b375359ca371211c6a453dccf
Misha Gorodnitzky <misaka@dmfg.io>
2024-01-13 09:14:38 +0000

    Add show/hide answers to number line puzzle

--- app/components/number_line_puzzle_component.rb
+++ app/components/number_line_puzzle_component.rb
@@ -1,9 +1,10 @@
 class NumberLinePuzzleComponent < ViewComponent::Base
-  attr_reader :puzzle
+  attr_reader :puzzle, :show_answers
 
-  def initialize(puzzle:)
+  def initialize(puzzle:, show_answers: false)
     super
 
     @puzzle = puzzle
+    @show_answers = show_answers
   end
 end
